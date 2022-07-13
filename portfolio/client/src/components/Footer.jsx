3b3ffb3e460c12f2b30dd49221bb9c5af656378f
rupeople <david.chamurliev@gmail.com>
2022-07-13 13:59:03 +1000

    footer container

--- portfolio/client/src/components/Footer.jsx
+++ portfolio/client/src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Container} from "react-bootstrap";
 
 const Footer = () => {
     return (
-        <footer className="footer d-flex flex-column align-items-center justify-content-center mb-2">
-            <span className="my-1">© 2022 David chamurliev. All Rights Reserved.</span>
-            <span className="my-1">3d model <a href="https://skfb.ly/6WxVW">"Shiba"</a> by zixisun02 is licensed under <a href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution</a></span>
+        <footer  className="footer container d-flex flex-column align-items-center justify-content-center mb-2">
+            <Container style={{maxWidth: 576}}>
+                <span className="my-1">© 2022 David chamurliev. All Rights Reserved.</span>
+                <span className="my-1">3d model <a href="https://skfb.ly/6WxVW">"Shiba"</a> by zixisun02 is licensed under <a href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution</a></span>
+            </Container>
         </footer>
     );
 };
